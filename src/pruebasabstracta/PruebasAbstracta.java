bc43c932e001d486dcf4a3fbeae3fa21524bfa57
jimfede <jimfede@gmail.com>
2018-02-19 11:21:42 -0300

    primer ejercicio

--- src/pruebasabstracta/PruebasAbstracta.java
+++ src/pruebasabstracta/PruebasAbstracta.java
@@ -5,6 +5,10 @@
  */
 package pruebasabstracta;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.System.console;
+import java.util.ArrayList;
+
 /**
  *
  * @author SALTI002
@@ -15,7 +19,31 @@ public class PruebasAbstracta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        ArrayList<Integer> lstNumeros = new ArrayList<>();
+        lstNumeros.add(5);
+        lstNumeros.add(1);
+        lstNumeros.add(1);
+
+        System.out.println(suma(lstNumeros));
+//        numeros = System.console().readLine();
+//        suma(numeros);
+    }
+
+    public static Integer sumaFor(ArrayList<Integer> lstNumeros) {
+        int resultado = 0;
+        for (Integer i = 0;  i < lstNumeros.size(); i++) {
+            resultado += lstNumeros.get(i);
+        }
+        return resultado;
     }
     
+    public static Integer sumaWhile(ArrayList<Integer> lstNumeros){
+        int resultado = 0;
+        while (){
+        
+        }
+        return resultado;
+    }
+
 }
